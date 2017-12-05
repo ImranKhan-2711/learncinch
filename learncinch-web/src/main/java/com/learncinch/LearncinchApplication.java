6b62a467481dba92196d11470509b98a93daef24
Mandeep <amritpal.insonix@gmail.com>
2017-12-05 13:32:32 +0530

    initial commit

---
+++ learncinch-web/src/main/java/com/learncinch/LearncinchApplication.java
@@ -0,0 +1,13 @@
+package com.learncinch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearncinchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearncinchApplication.class, args);
+	}
+
+}
